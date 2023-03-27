16ba490c48f27e24af7c226ae3e9dd3cb244140f
Oggal <Philip.dan.taylor@gmail.com>
2023-03-27 05:08:13 -0700

    Update State

--- models/state.py
+++ models/state.py
@@ -7,28 +7,31 @@ import os
 
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    
+   
     class State(BaseModel, Base):
         """ State class """
+        from sqlalchemy.orm import relationship
         __tablename__ = "states"
         name = Column(String(128), nullable=False)
 
-        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-            from sqlalchemy.orm import relationship
-            cities = relationship("City", backref="state",
-                                  cascade="all, delete")
-        else:
-            @property
-            def cities(self):
-                """getter attribute cities that returns the list of City
-                with state_id equals to the current State.id"""
-                from models import storage
-                list_cities = []
-                for key, value in storage.all(City).items():
-                    if value.state_id == self.id:
-                        list_cities.append(value)
-                return list_cities
+        cities = relationship("City", backref="state",
+                                cascade="all, delete")
+
+
 else:
 
     class State(BaseModel):
         '''File Storage State Class'''
         name = ""
+
+        @property
+        def cities(self):
+            """getter attribute cities that returns the list of City
+            with state_id equals to the current State.id"""
+            from models import storage
+            list_cities = []
+            for key, value in storage.all(City).items():
+                if value.state_id == self.id:
+                    list_cities.append(value)
+            return list_cities
