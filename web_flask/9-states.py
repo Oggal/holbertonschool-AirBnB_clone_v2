3e78970a184c27eaf25b16e4ff50a8aa9edeb2ff
Oggal <Philip.dan.taylor@gmail.com>
2023-03-27 12:04:32 -0700

    Task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Start a Flask web application """
+
+from flask import Flask
+from flask import render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+def StartFlask():
+    """ Start a Flask web application """
+    app.run(host='0.0.0.0', port=5000)
+
+
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """ Display List of States """
+    states = storage.all('State')
+    return render_template('9-states.html', states=states.values(), id=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """ Display List of States """
+    states = storage.all('State')
+    state = states.get('State.' + id)
+    return render_template('9-states.html', states=state, my_id=id)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    StartFlask()
