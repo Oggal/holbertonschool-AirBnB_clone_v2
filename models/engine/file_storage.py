1c0e8ab319e0a6d0e9e78075a5ee6f4af9b0d507
Oggal <Philip.dan.taylor@gmail.com>
2023-03-27 08:51:43 -0700

    Task 7, FileStorgae Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
-
+    __all_classes = {"state": State, "city": City, "amenity": Amenity,
+                     "place": Place, "review": Review, "user": User}
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if type(cls) is str:
+            cls = self.__all_classes.get(cls.lower())
         if cls is None:
             return FileStorage.__objects
         return {k: v for k, v in FileStorage.__objects.items()
@@ -17,7 +27,7 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.__objects.update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -44,14 +54,6 @@ class FileStorage:
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
         classes = {
                     'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City, 'Amenity': Amenity,
