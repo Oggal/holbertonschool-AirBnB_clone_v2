7f14a6c708c6a72f32a114d9bc468bd6f9d1af34
Oggal <Philip.dan.taylor@gmail.com>
2023-03-27 05:29:28 -0700

    Pycodestyle

--- models/state.py
+++ models/state.py
@@ -7,8 +7,7 @@ import os
 
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    
-   
+
     class State(BaseModel, Base):
         """ State class """
         from sqlalchemy.orm import relationship
@@ -16,8 +15,7 @@ if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
 
         cities = relationship("City", backref="state",
-                                cascade="all, delete")
-
+                              cascade="all, delete")
 
 else:
 
